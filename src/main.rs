3feeba3fecffc3feafa980e636e1eb917c87a882
Luiz Schonarth <luizcschonarth@gmail.com>
2023-07-17 20:23:12 -0300

    Simple ping pong commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use dotenv::dotenv;
+use serenity::model::gateway::Ready;
+use serenity::async_trait;
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{ command, group };
+use serenity::framework::standard::{ StandardFramework, CommandResult };
+
+#[group] 
+#[commands(ping, pong)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("."))
+        .group(&GENERAL_GROUP);
+
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("An error occured while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+    Ok(())
+}
+
+#[command]
+async fn pong(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Ping!").await?;
+    Ok(())
 }

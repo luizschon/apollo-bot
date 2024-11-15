b9525cafc08d0236e0fb273fb9658ed232eb9f52
Luiz Schonarth <67851507+luizschon@users.noreply.github.com>
2024-11-15 13:23:58 -0300

    lavalink: uses lavalink (#5)
    - **Use lavalink_rs**
    - **Added lavalink container**

--- src/commands.rs
+++ src/commands.rs
@@ -1,7 +1,178 @@
 use crate::Context;
 
-#[poise::command(prefix_command, slash_command)]
+use std::ops::Deref;
+
+use anyhow::anyhow;
+use lavalink_rs::prelude::*;
+use poise::serenity_prelude as serenity;
+use serenity::{model::id::ChannelId, Http, Mentionable};
+
+#[poise::command(slash_command)]
 pub async fn ping(ctx: Context<'_>) -> anyhow::Result<()> {
     ctx.say("Pong!").await?;
+    Ok(())
+}
+async fn _join(
+    ctx: &Context<'_>,
+    guild_id: serenity::GuildId,
+    channel_id: Option<serenity::ChannelId>,
+) -> anyhow::Result<bool> {
+    let lava_client = ctx.data().lavalink.clone();
+
+    let manager = songbird::get(ctx.serenity_context()).await.unwrap().clone();
+
+    if lava_client.get_player_context(guild_id).is_none() {
+        let connect_to = match channel_id {
+            Some(x) => x,
+            None => {
+                let guild = ctx.guild().unwrap().deref().clone();
+                let user_channel_id = guild
+                    .voice_states
+                    .get(&ctx.author().id)
+                    .and_then(|voice_state| voice_state.channel_id);
+
+                match user_channel_id {
+                    Some(channel) => channel,
+                    None => {
+                        ctx.say("Not in a voice channel").await?;
+
+                        return Err(anyhow!("Not in a voice channel"));
+                    }
+                }
+            }
+        };
+
+        let handler = manager.join_gateway(guild_id, connect_to).await;
+
+        match handler {
+            Ok((connection_info, _)) => {
+                lava_client
+                    // The turbofish here is Optional, but it helps to figure out what type to
+                    // provide in `PlayerContext::data()`
+                    //
+                    // While a tuple is used here as an example, you are free to use a custom
+                    // public structure with whatever data you wish.
+                    // This custom data is also present in the Client if you wish to have the
+                    // shared data be more global, rather than centralized to each player.
+                    .create_player_context_with_data::<(ChannelId, std::sync::Arc<Http>)>(
+                        guild_id,
+                        connection_info,
+                        std::sync::Arc::new((
+                            ctx.channel_id(),
+                            ctx.serenity_context().http.clone(),
+                        )),
+                    )
+                    .await?;
+
+                ctx.say(format!("Joined {}", connect_to.mention())).await?;
+
+                return Ok(true);
+            }
+            Err(why) => {
+                ctx.say(format!("Error joining the channel: {}", why))
+                    .await?;
+                return Err(why.into());
+            }
+        }
+    }
+
+    Ok(false)
+}
+
+#[poise::command(slash_command, prefix_command)]
+pub async fn play(
+    ctx: Context<'_>,
+    #[description = "Search term or URL"]
+    #[rest]
+    term: Option<String>,
+) -> anyhow::Result<()> {
+    let guild_id = ctx.guild_id().unwrap();
+
+    let has_joined = _join(&ctx, guild_id, None).await?;
+
+    let lava_client = ctx.data().lavalink.clone();
+
+    let Some(player) = lava_client.get_player_context(guild_id) else {
+        ctx.say("Join the bot to a voice channel first.").await?;
+        return Ok(());
+    };
+
+    let query = dbg!(if let Some(term) = term {
+        if term.starts_with("http") {
+            term
+        } else {
+            //SearchEngines::YouTube.to_query(&term)?
+            SearchEngines::SoundCloud.to_query(&term)?
+        }
+    } else {
+        if let Ok(player_data) = player.get_player().await {
+            let queue = player.get_queue();
+
+            if player_data.track.is_none() && queue.get_track(0).await.is_ok_and(|x| x.is_some()) {
+                player.skip()?;
+            } else {
+                ctx.say("The queue is empty.").await?;
+            }
+        }
+
+        return Ok(());
+    });
+
+    let loaded_tracks = lava_client.load_tracks(guild_id, &query).await?;
+
+    let mut playlist_info = None;
+
+    let mut tracks: Vec<TrackInQueue> = match loaded_tracks.data {
+        Some(TrackLoadData::Track(x)) => vec![x.into()],
+        Some(TrackLoadData::Search(x)) => vec![x[0].clone().into()],
+        Some(TrackLoadData::Playlist(x)) => {
+            playlist_info = Some(x.info);
+            x.tracks.iter().map(|x| x.clone().into()).collect()
+        }
+
+        _ => {
+            ctx.say(format!("{:?}", loaded_tracks)).await?;
+            return Ok(());
+        }
+    };
+
+    if let Some(info) = playlist_info {
+        ctx.say(format!("Added playlist to queue: {}", info.name,))
+            .await?;
+    } else {
+        let track = &tracks[0].track;
+
+        if let Some(uri) = &track.info.uri {
+            ctx.say(format!(
+                "Added to queue: [{} - {}](<{}>)",
+                track.info.author, track.info.title, uri
+            ))
+            .await?;
+        } else {
+            ctx.say(format!(
+                "Added to queue: {} - {}",
+                track.info.author, track.info.title
+            ))
+            .await?;
+        }
+    }
+
+    for i in &mut tracks {
+        i.track.user_data = Some(serde_json::json!({"requester_id": ctx.author().id.get()}));
+    }
+
+    let queue = player.get_queue();
+    queue.append(tracks.into())?;
+
+    if has_joined {
+        return Ok(());
+    }
+
+    if let Ok(player_data) = player.get_player().await {
+        if player_data.track.is_none() && queue.get_track(0).await.is_ok_and(|x| x.is_some()) {
+            player.skip()?;
+        }
+    }
+
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,71 @@
 use std::env;
 
 use dotenv::dotenv;
+use lavalink_rs::{client::LavalinkClient, node::NodeBuilder, prelude::*};
 use poise::serenity_prelude as serenity;
 use poise::{Framework, FrameworkOptions};
 use serenity::{all::ClientBuilder, prelude::*};
+use songbird::SerenityInit;
 
 mod commands;
 
 type Context<'a> = poise::Context<'a, Data, anyhow::Error>;
 
-struct Data;
+struct Data {
+    pub lavalink: LavalinkClient,
+}
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     dotenv().ok();
 
     let options = FrameworkOptions {
-        commands: vec![commands::ping()],
+        commands: vec![commands::ping(), commands::play()],
         ..Default::default()
     };
 
     let framework = Framework::builder()
-        .setup(move |_ctx, ready, _framework| {
+        .options(options)
+        .setup(move |ctx, _ready, framework| {
             Box::pin(async move {
-                tracing::debug!("Logged in as {}", ready.user.name);
-                Ok(Data)
+                poise::builtins::register_globally(&ctx.http, &framework.options().commands)
+                    .await?;
+
+                let node_local = NodeBuilder {
+                    hostname: format!(
+                        "localhost:{}",
+                        env::var("LAVALINK_PORT")
+                            .expect("Expected LAVALINK_PORT in the environment")
+                    ),
+                    is_ssl: false,
+                    events: Default::default(),
+                    password: env::var("LAVALINK_PASS")
+                        .expect("Expected LAVALINK_PASS in the environment"),
+                    user_id: ctx.cache.current_user().id.into(),
+                    session_id: None,
+                };
+
+                Ok(Data {
+                    lavalink: LavalinkClient::new(
+                        Default::default(),
+                        vec![node_local],
+                        NodeDistributionStrategy::round_robin(),
+                    )
+                    .await,
+                })
             })
         })
-        .options(options)
         .build();
 
     let token = env::var("DISCORD_TOKEN").expect("Expected DISCORD_TOKEN in the environment");
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
 
     let mut client = ClientBuilder::new(token, intents)
+        .register_songbird()
         .framework(framework)
-        .await
-        .unwrap();
+        .await?;
 
-    if let Err(why) = client.start().await {
-        tracing::error!("Client error: {why:?}");
-    }
+    client.start().await?;
+    Ok(())
 }
